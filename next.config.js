1a210833c18b3c51dc81ef8932ea77bb182cb338
mojoblue7 <lsh18542@gmail.com>
2021-12-23 02:47:53 +0900

    임시 커밋

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+const isProduction = process.env.NODE_ENV === 'production';
+const productionURL = 'https://mojoblue7.github.io';
+
 module.exports = {
   reactStrictMode: true,
-}
+  assetPrefix: isProduction ? productionURL : '',
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  images: {
+    loader: 'imgix',
+    path: productionURL,
+  },
+  webpack(config, { isServer, buildId }) {
+    config.resolve = {
+      alias: {
+        '@src': path.join(__dirname, 'src'),
+        '@components': path.join(__dirname, 'src', 'components'),
+        '@containers': path.join(__dirname, 'src', 'containers'),
+        '@store': path.join(__dirname, 'src', 'store'),
+        // '@interface': path.join(__dirname, 'src', '@types/interface.ts'),
+      },
+      ...config.resolve,
+    };
+
+    return config;
+  },
+};
