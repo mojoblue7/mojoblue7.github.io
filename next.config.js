c96ef50d050ff2a7b73711238b441c58d80d14ed
mojoblue7 <lsh18542@gmail.com>
2021-12-24 04:16:07 +0900

    no message

--- next.config.js
+++ next.config.js
@@ -2,24 +2,15 @@
 
 const path = require('path');
 const isProduction = process.env.NODE_ENV === 'production';
+const localURL = 'http://localhost:3000/';
 const productionURL = 'https://mojoblue7.github.io';
 
 module.exports = {
   reactStrictMode: true,
-  // assetPrefix: isProduction ? productionURL : '',
+  assetPrefix: isProduction ? productionURL : localURL,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  webpack(config, { isServer, buildId }) {
-    config.resolve = {
-      alias: {
-        '@src': path.join(__dirname, 'src'),
-        '@components': path.join(__dirname, 'src', 'components'),
-        '@containers': path.join(__dirname, 'src', 'containers'),
-        '@store': path.join(__dirname, 'src', 'store'),
-        // '@interface': path.join(__dirname, 'src', '@types/interface.ts'),
-      },
-      ...config.resolve,
-    };
-
-    return config;
+  images: {
+    loader: 'imgix',
+    path: isProduction ? productionURL : localURL,
   },
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '@styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
@@ -62,7 +62,7 @@ const Home: NextPage = () => {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src={`/vercel.svg`} alt="Vercel Logo" width={72} height={16} />
+            <Image src={`/vercel-test.svg`} alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
